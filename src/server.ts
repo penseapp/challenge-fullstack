a871410fdc8b94c214e095489520e9239191622d
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2021-05-30 01:37:13 -0300

    change server port

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import app from "./app"
 
 dotenv.config()
 
-app.listen(process.env.PROD_DB_PORT || 5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${process.env.PROD_DB_PORT || '5000'}`)
 })
